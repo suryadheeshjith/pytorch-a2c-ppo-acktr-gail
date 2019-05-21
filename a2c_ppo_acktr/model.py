46454a67f02245b19f9d621a0e05d936e68932a8
ankeshanand <ankeshanand1994@gmail.com>
2019-05-21 17:51:47 -0400

    Update model for 210, 160

--- a2c_ppo_acktr/model.py
+++ a2c_ppo_acktr/model.py
@@ -176,8 +176,9 @@ class CNNBase(NNBase):
         self.main = nn.Sequential(
             init_(nn.Conv2d(num_inputs, 32, 8, stride=4)), nn.ReLU(),
             init_(nn.Conv2d(32, 64, 4, stride=2)), nn.ReLU(),
-            init_(nn.Conv2d(64, 32, 3, stride=1)), nn.ReLU(), Flatten(),
-            init_(nn.Linear(32 * 7 * 7, hidden_size)), nn.ReLU())
+            init_(nn.Conv2d(64, 128, 4, stride=2)), nn.ReLU(),
+            init_(nn.Conv2d(128, 64, 3, stride=1)), nn.ReLU(), Flatten(),
+            init_(nn.Linear(64 * 9 * 6, hidden_size)), nn.ReLU())
 
         init_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.
                                constant_(x, 0))
