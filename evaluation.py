7968f18bca7d9c974cf856983e49ddd1690ab1d2
ankeshanand <ankeshanand1994@gmail.com>
2019-05-22 21:08:34 -0400

    Fix a stupid bug

--- evaluation.py
+++ evaluation.py
@@ -24,7 +24,7 @@ def evaluate(actor_critic, ob_rms, env_name, seed, num_processes, eval_log_dir,
 
     while len(eval_episode_rewards) < 10:
         with torch.no_grad():
-            _, action, _, eval_recurrent_hidden_states = actor_critic.act(
+            _, action, _, eval_recurrent_hidden_states, _, _ = actor_critic.act(
                 obs,
                 eval_recurrent_hidden_states,
                 eval_masks,
