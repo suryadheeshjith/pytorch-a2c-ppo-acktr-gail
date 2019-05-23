e1c9d232c34a9d03e94d10e8b6eaa8ee194a9b1c
ankeshanand <ankeshanand1994@gmail.com>
2019-05-23 02:37:48 -0400

    Apply relu on non-relu actor features when computing action.

--- a2c_ppo_acktr/model.py
+++ a2c_ppo_acktr/model.py
@@ -53,7 +53,7 @@ class Policy(nn.Module):
 
     def act(self, inputs, rnn_hxs, masks, deterministic=False):
         value, actor_features, rnn_hxs = self.base(inputs, rnn_hxs, masks)
-        dist = self.dist(actor_features)
+        dist = self.dist(F.relu(actor_features))
 
         if deterministic:
             action = dist.mode()
